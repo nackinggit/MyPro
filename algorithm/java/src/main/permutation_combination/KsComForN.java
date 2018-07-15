e0c4a856db19511588b0ca6d6dc82763be698677
nacking <p375242380@163.com>
2018-07-15 18:50:58 +0800

    init

---
+++ algorithm/java/src/main/permutation_combination/KsComForN.java
@@ -0,0 +1,36 @@
+package permutation_combination;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * C(n,k)问题， 即n个不同数中取k个的组合
+ * 本质上是深度遍历
+ */
+public class KsComForN {
+    public static List<List<Integer>> cnk(int n, int k) {
+        List<List<Integer>> res = new LinkedList<>();
+        List<Integer> curRes = new LinkedList<>();
+        doCombination(res, curRes, n, k, 1);
+        return res;
+    }
+
+    private static void doCombination(List<List<Integer>> res, List<Integer> curRes, int n, int k, int startVal) {
+        if (n < 1 || k > n) throw new IllegalArgumentException(n + "," + k);
+
+        if (curRes.size() == k) {
+            res.add(new LinkedList<>(curRes));
+        } else {
+            for (int i = startVal; i <= n; i++) {
+                curRes.add(i);
+                doCombination(res, curRes, n, k, i + 1);
+                curRes.remove(curRes.size() - 1);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(cnk(3, 2));
+    }
+}
---
+++ algorithm/java/src/main/permutation_combination/NPermutation.java
@@ -0,0 +1,48 @@
+package permutation_combination;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * N个数的全排列, 大小为n的数字数组的全排列
+ */
+public class NPermutation {
+    public static List<List<Integer>> nPerm(int[] nums) {
+        if (nums == null || nums.length == 0) return null;
+
+        List<List<Integer>> res = new LinkedList<>();
+        doNPerm(res, nums, 0, nums.length);
+        return res;
+    }
+
+    private static void doNPerm(List<List<Integer>> res, int[] nums, int startIndex, int endIndex) {
+        if (startIndex == endIndex - 1) {
+            List<Integer> one = Arrays.stream(nums).boxed().collect(Collectors.toList());
+            res.add(one);
+        }
+
+        for (int k = startIndex; k < endIndex; k++) {
+            if(k == startIndex || nums[startIndex] != nums[k]) {
+                swap(nums, startIndex, k);
+                doNPerm(res, nums, startIndex + 1, endIndex);
+                swap(nums, startIndex, k);
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int i, int k) {
+        if (nums[i] != nums[k]) {
+            int temp = nums[i];
+            nums[i] = nums[k];
+            nums[k] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 2, 3};
+        System.out.println(nPerm(nums));
+    }
+
+}
