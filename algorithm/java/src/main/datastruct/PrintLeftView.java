7af27f6561759862f93c90e147d65281efa00204
nacking <p375242380@163.com>
2019-04-05 11:03:14 +0800

    add

---
+++ algorithm/java/src/main/datastruct/PrintLeftView.java
@@ -0,0 +1,58 @@
+package datastruct;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
+
+public class PrintLeftView {
+
+    static void printLeftView(TreeNode root) {
+        if (root != null) {
+            Deque<TreeNode> queue = new ArrayDeque<>();
+            queue.add(root);
+            queue.add(TreeNode.EMPTY);
+            while (!queue.isEmpty()) {
+                TreeNode first = queue.poll();
+                if (first != null) {
+                    if(first != TreeNode.EMPTY) {
+                        System.out.print(first.val);
+                    } else {
+                        System.out.print(", ");
+                    }
+
+                    if (first.left != null) {
+                        queue.add(first.left);
+                    }
+
+                    if (first.right != null) {
+                        queue.add(first.right);
+                    }
+
+                    while (true) {
+                        TreeNode node = queue.poll();
+                        if(node != null && node != TreeNode.EMPTY) {
+//                            System.out.print(node.val);
+                            if(node.left != null) {
+                                queue.add(node.left);
+                            }
+                            if(node.right != null) {
+                                queue.add(node.right);
+                            }
+                        } else {
+//                            System.out.print(",");
+                            if(!queue.isEmpty()) {
+                                queue.add(TreeNode.EMPTY);
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        printLeftView(TreeNode.TREE);
+    }
+}
---
+++ algorithm/java/src/main/datastruct/TreeNode.java
@@ -0,0 +1,45 @@
+package datastruct;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public static final TreeNode EMPTY = new TreeNode(Integer.MIN_VALUE);
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+
+    public static final TreeNode TREE = tree();
+
+    private static TreeNode tree() {
+        TreeNode root = new TreeNode(1);
+        int i = 0;
+//        Deque<TreeNode> deque = new ArrayDeque<>();
+//        deque.add(root);
+//        while (i++ < 10) {
+//            TreeNode node = deque.poll();
+//            if(node != null) {
+//                TreeNode left = new TreeNode(node.val + 1);
+//                TreeNode right = new TreeNode(node.val + 2);
+//                node.left = left;
+//                node.right = right;
+//                deque.add(left);
+//                deque.add(right);
+//            }
+//        }
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        root.right.right.right = new TreeNode(8);
+        return root;
+    }
+}
